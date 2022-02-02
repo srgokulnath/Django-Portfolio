2f33193de8f80f65f6d242fcd65a2377ad8ed242
srgokulnath <srgwilet@gmail.com>
2022-02-02 13:07:26 +0530

    design, urls

--- jobs/models.py
+++ jobs/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to = 'images/')
     summary = models.CharField(max_length = 500)
+
+    def __str__(self):
+        return self.summary
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,9 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR
+
+
+
 
 
